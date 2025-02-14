08b800240915989797c31d7ea0b92017818a2764
Sabo <kaleb.sabo.25@gmail.com>
2025-02-14 10:03:47 +0000

    Add initial Tkinter setup for Game1

---
+++ Game1/main.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    root.title("Game1")
+    root.geometry("800x600")
+    root.mainloop()
--- main.py
+++
@@ -1,4 +0,0 @@
-from tkinter import *
-
-if __name__ == '__main__':
-    pass
